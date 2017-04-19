08e196f47e286afa659b82254bc5a217fff3c97f
thaisviana <thais@hub.co>
2017-04-19 00:46:13 -0300

    testing heroku deploy

--- community/views.py
+++ community/views.py
@@ -4,5 +4,6 @@ from django.shortcuts import render
 def index(request):
     c = {}
     c['title'] = 'comunidade de programação'
+    c['oi'] = 'oi'
     c['explanation'] = 'O projeto “comunidade de programação” visa alavancar a aprendizagem de programação de jovens e adultos através de abordagens democráticas, e explorar maneiras de tornar a programação acessível a um público numeroso e diverso usando e-learning.'
     return render(request, "index.html", c)
